5715fd0400280ce97f7b56261a86e67f3ad349fb
koesnam <76582376+10kseok@users.noreply.github.com>
2024-01-11 04:52:13 +0900

    feat: 마이페이지 미리보기 구현

--- static/myPage/myPage.js
+++ static/myPage/myPage.js
@@ -1,3 +1,4 @@
+
 const requiredFields = document.getElementsByClassName('required')
 const saveBtn = document.getElementById("saveForm")
 
@@ -10,4 +11,34 @@ saveBtn.addEventListener('submit', function (event) {
             return;
         }
     }
+});
+
+// 팝업 열기 버튼에 이벤트 리스너 추가
+document.getElementById('previewBtn').addEventListener('click', function() {
+    // 팝업과 오버레이를 보이게 설정
+    document.getElementById('popup').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+    const previewContent = document.getElementById('previewContent');
+
+    const inputTags = document.getElementsByTagName('input')
+    for (const tag of inputTags) {
+        if (tag.value) {
+            const info = document.createElement('li');
+            info.className = 'infoLine';
+            const korTagName = $(`label[for='${tag.name}']`).text()
+            info.innerHTML = `
+                <span class='labelText'>${korTagName}</span>
+                <span class='valueText'>${tag.value}</span>
+            `;
+            previewContent.appendChild(info)
+        }
+    }
+});
+
+// 팝업 닫기 버튼에 이벤트 리스너 추가
+document.getElementById('closePopupBtn').addEventListener('click', function() {
+    // 팝업과 오버레이를 감추게 설정
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('previewContent').innerHTML = '';
 });
