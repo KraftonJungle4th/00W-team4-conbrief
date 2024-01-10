a1d6f1ee21abf5ef5ba322eba5e62fcf8554fd42
koesnam <76582376+10kseok@users.noreply.github.com>
2024-01-10 11:19:02 +0900

    feat: 인증 필터 구현
    페이지, api에서 인증 기능을 구현했습니다.
    비로그인시 로그인 화면으로 리다이렉트 되게끔 설정해뒀습니다.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+
+from flask import Flask, abort, redirect, render_template, request, url_for
 
 from controller.login import login_bp
 from controller.signup import signup_bp
 from controller.user import user_bp
+from validators import TokenValidator
 
 app = Flask(__name__)
 
@@ -14,3 +16,28 @@ app.register_blueprint(user_bp)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.before_request
+def filter():
+    if request.endpoint == 'static':
+        return
+    
+    # api 인증 처리
+    if request.path.startswith("/api"):
+        if not request.headers.get('Authorization', None):
+            return abort(401)
+        
+        tokenType, token = request.headers['Authorization'].split()
+        try:
+            if tokenType != 'Bearer' or TokenValidator.validateToken(token):
+                return abort(401)
+        except:
+            return abort(401)
+        
+    # 페이지 인증 처리
+    if request.path == '/login' and request.cookies.get('accessToken', None):
+        return redirect(url_for('home'))
+    elif request.path != '/login' and request.path != '/signup' and not TokenValidator.validateAuthReq(request):
+        return redirect(url_for('login.login'))
+    
