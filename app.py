bb455819b2bc66110c373b8f43933137f884557f
pkw-May <pkw.may@gmail.com>
2024-01-10 15:22:44 +0900

    Add: main page searching area

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, abort, redirect, render_template, request, url_for
 
 from controller.login import login_bp
 from controller.signup import signup_bp
+from controller.main import main_bp
 from controller.user import user_bp
 from validators import TokenValidator
 
@@ -10,6 +11,7 @@ app = Flask(__name__)
 
 app.register_blueprint(login_bp)
 app.register_blueprint(signup_bp)
+app.register_blueprint(main_bp)
 app.register_blueprint(user_bp)
 
 
@@ -22,22 +24,21 @@ def home():
 def filter():
     if request.endpoint == 'static':
         return
-    
+
     # api 인증 처리
     if request.path.startswith("/api"):
         if not request.headers.get('Authorization', None):
             return abort(401)
-        
+
         tokenType, token = request.headers['Authorization'].split()
         try:
             if tokenType != 'Bearer' or TokenValidator.validateToken(token):
                 return abort(401)
         except:
             return abort(401)
-        
+
     # 페이지 인증 처리
     if request.path == '/login' and request.cookies.get('accessToken', None):
         return redirect(url_for('home'))
     elif request.path != '/login' and request.path != '/signup' and not TokenValidator.validateAuthReq(request):
         return redirect(url_for('login.login'))
-    
---
+++ static/main/main.js
@@ -0,0 +1,6 @@
+function searchStudenNo () {
+    const searchNoBtn = document.getElementById('searchNo');
+    
+    // 수강생 정보 GET API
+
+};
