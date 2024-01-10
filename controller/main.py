0414ec331b43cfb80846b39ffb4f1373c68a9f4e
pkw-May <pkw.may@gmail.com>
2024-01-10 21:45:57 +0900

    add: main page info ui

--- controller/main.py
+++ controller/main.py
@@ -4,22 +4,18 @@ import jwt
 from config.TokenProperty import TokenProperty
 from model.Token import Token
 from repository.UserRepository import UserRepository
-from clientConfigs.main.CONFIGS import SEARCH_CONFIG
+from clientConfigs.main.CONFIGS import SEARCH_CONFIG, INFO_CONFIG
 
 userRepository = UserRepository()
 main_bp = Blueprint("main", __name__)
 
 
 @main_bp.route("/main/<studentNo>", methods=['GET', 'POST'])
-def main(studentNo:str):
+def main(studentNo: str):
     if request.method == 'GET':
-        
-        return render_template('main/main.html', SEARCH_CONFIG=SEARCH_CONFIG )
+
+        return render_template('main/main.html', SEARCH_CONFIG=SEARCH_CONFIG)
 
     if request.method == 'POST':
-
-        RESULT = userRepository.findByStudentNo(studentNo = studentNo)
-        return render_template('main/main.html',SEARCH_CONFIG =SEARCH_CONFIG,RESULT=RESULT)
-        
-        
-        
+        RESULT = userRepository.findByStudentNo(studentNo=studentNo)
+        return render_template('main/main.html', SEARCH_CONFIG=SEARCH_CONFIG, RESULT=RESULT, INFO_CONFIG=INFO_CONFIG)
