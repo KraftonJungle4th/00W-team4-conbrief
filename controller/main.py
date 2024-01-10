e2f9623c4d93dd290b9d65dc18a8a91df3ff86ef
pkw-May <pkw.may@gmail.com>
2024-01-10 21:47:40 +0900

    add: pass game button config from flask to jinja

--- controller/main.py
+++ controller/main.py
@@ -4,7 +4,7 @@ import jwt
 from config.TokenProperty import TokenProperty
 from model.Token import Token
 from repository.UserRepository import UserRepository
-from clientConfigs.main.CONFIGS import SEARCH_CONFIG, INFO_CONFIG
+from clientConfigs.main.CONFIGS import SEARCH_CONFIG, INFO_CONFIG, GAME_BTN_CONFIG
 
 userRepository = UserRepository()
 main_bp = Blueprint("main", __name__)
@@ -18,4 +18,4 @@ def main(studentNo: str):
 
     if request.method == 'POST':
         RESULT = userRepository.findByStudentNo(studentNo=studentNo)
-        return render_template('main/main.html', SEARCH_CONFIG=SEARCH_CONFIG, RESULT=RESULT, INFO_CONFIG=INFO_CONFIG)
+        return render_template('main/main.html', SEARCH_CONFIG=SEARCH_CONFIG, RESULT=RESULT, INFO_CONFIG=INFO_CONFIG, GAME_BTN_CONFIG=GAME_BTN_CONFIG)
