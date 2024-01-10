ad6fa06b1c605116522978462623982d9e5accde
koesnam <76582376+10kseok@users.noreply.github.com>
2024-01-11 00:40:02 +0900

    Revert "Merge pull request #22 from jungle4th-00w-conbrief/feature-enter_my_page"
    This reverts commit c68ef3487986e9efc509e83f69e0a41944ec853f, reversing
    changes made to f2640bb35d70d11846e87a423adb5415e3e90aad.

--- repository/UserRepository.py
+++ repository/UserRepository.py
@@ -24,6 +24,7 @@ class UserRepository:
         return student
     
     def updateStudentInfor(self,studentInforDto:dict,studentNo:str):
+        
         self.__db.update_one({"studentNo":studentNo},{'$set':studentInforDto})
 
 
