114cee8de37c2dbde14dcd44b8121ed09a3f1186
mkae21 <bbuchu123@gmail.com>
2024-01-10 21:12:38 +0900

    feat: mainPage 정보 조회
    GET 요청 처리, main request.metod =='POST' 추가, GAME_BTN_CONFIG 추가

--- clientConfigs/main/CONFIGS.py
+++ clientConfigs/main/CONFIGS.py
@@ -1,4 +1,5 @@
 SEARCH_CONFIG = {
+
     'introduction': '궁금한 수강생의 번호를 입력해보세요!',
     'name': 'searchNo',
     'type': 'text',
@@ -6,4 +7,12 @@ SEARCH_CONFIG = {
     'placeholder': 'ex. 4기-06의 경우 0406',
     'btnName': '조회',
     'btnId': 'searchNoBtn'
+    
+}
+
+GAME_BTN_CONFIG ={
+    
+    'btnName' :'미니 게임 시작',
+    'btnId':'miniStartBtn'
+
 }
