1c55021e7330b289b64a78a79abca7fb0a7727eb
pkw-May <pkw.may@gmail.com>
2024-01-11 10:25:45 +0900

    add mypage preview logic

--- static/myPage/myPage.js
+++ static/myPage/myPage.js
@@ -1,10 +1,9 @@
+const requiredFields = document.getElementsByClassName("required");
+const saveBtn = document.getElementById("saveForm");
 
-const requiredFields = document.getElementsByClassName('required')
-const saveBtn = document.getElementById("saveForm")
-
-saveBtn.addEventListener('submit', function (event) {
+saveBtn.addEventListener("submit", function (event) {
     for (const field of requiredFields) {
-        inputValue = field.value
+        inputValue = field.value;
         if (!new RegExp(field.pattern).test(inputValue)) {
             alert("이름, 이메일, 포부, 깃헙아이디는 필수 항목입니다.");
             event.preventDefault();
@@ -13,32 +12,51 @@ saveBtn.addEventListener('submit', function (event) {
     }
 });
 
-// 팝업 열기 버튼에 이벤트 리스너 추가
-document.getElementById('previewBtn').addEventListener('click', function() {
-    // 팝업과 오버레이를 보이게 설정
-    document.getElementById('popup').style.display = 'block';
-    document.getElementById('overlay').style.display = 'block';
-    const previewContent = document.getElementById('previewContent');
+function closePreviewModal () {
+    $("#previewModal").remove();
+}
 
-    const inputTags = document.getElementsByTagName('input')
+function openPreview() {
+    const inputTags = document.getElementsByTagName("input");
+    let inputList = [];
     for (const tag of inputTags) {
         if (tag.value) {
-            const info = document.createElement('li');
-            info.className = 'infoLine';
-            const korTagName = $(`label[for='${tag.name}']`).text()
-            info.innerHTML = `
-                <span class='labelText'>${korTagName}</span>
-                <span class='valueText'>${tag.value}</span>
-            `;
-            previewContent.appendChild(info)
+            inputList = [...inputList, {
+                                        name: $(`label[for='${tag.name}']`).text().trim(),
+                                        value: tag.value
+                                    }
+                        ]
         }
     }
-});
+    $("#con-brief-my-page").append(MODAL_TEMP(inputList));
+}
 
-// 팝업 닫기 버튼에 이벤트 리스너 추가
-document.getElementById('closePopupBtn').addEventListener('click', function() {
-    // 팝업과 오버레이를 감추게 설정
-    document.getElementById('popup').style.display = 'none';
-    document.getElementById('overlay').style.display = 'none';
-    document.getElementById('previewContent').innerHTML = '';
-});
+const MODAL_TEMP = (inputList) => `
+        <div id='previewModal'>
+            <div class='modalOverlay'></div>
+            <div class='modalWrapper'>
+                <div class="modal">
+                    <div id='closeBtn' onclick='closePreviewModal()'>
+                        <i class="fa-regular fa-circle-xmark"></i>
+                    </div>
+                    <ul class='flexVertical'>
+                        ${inputList.map(
+                            item =>
+                                `<li class='infoLine'>
+                                      <div class='flexHorizontal lineElement'>
+                                          <div class='labelText'>
+                                              ${item.name}
+                                          </div>
+                                          <div class='valueText'>
+                                              ${item.value}
+                                          </div>
+                                      </div>     
+                                   </li>
+                                  `
+                            )                                
+                        }
+                    </ul>
+                </div>
+            </div>
+        </div>
+      `.replace(/,/g, '');
